616aa5da7cfafaf1f24ae8ec9f8e8b8a5770016f
Ertugrul Karademir <ekarademir@gmail.com>
2017-10-09 01:30:18 +0100

    Boilerplate ES2015

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,26 @@
 'use strict';
 const path = require('path');
 const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'tetr.dist.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
