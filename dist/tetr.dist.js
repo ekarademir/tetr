ade18e1acb9b871c1da25164e19c9a2ae9af32de
Ertugrul Karademir <ekarademir@gmail.com>
2017-10-12 09:02:24 +0100

    Daily commit

--- dist/tetr.dist.js
+++ dist/tetr.dist.js
@@ -99,14 +99,21 @@ tetr.setup('.main'); /**
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.setup = undefined;
+
+var _svg = __webpack_require__(2);
+
+var svg = _interopRequireWildcard(_svg);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 /**
  * Adds game screen to given DOM element.
  * @param {string} mainSelector DOM selector to insert game screen.
  */
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
 var setup = exports.setup = function setup(mainSelector) {
 
   var main = document.querySelector(mainSelector);
@@ -120,24 +127,213 @@ var setup = exports.setup = function setup(mainSelector) {
  */
 var mainScreen = function mainScreen(mainElement) {
 
-  var svg = createSVGElement('svg');
-  svg.setAttributeNS(null, 'width', '100%');
-  svg.setAttributeNS(null, 'height', '100%');
-  svg.setAttributeNS(null, 'preserveAspectRatio', 'xMinYMin');
-  svg.setAttributeNS(null, 'viewBox', '400, 300, 400, 300');
+  var canvas = svg.createSVGElement('svg', {
+    width: '100%',
+    height: '100%',
+    preserveAspectRatio: 'xMinYMin',
+    viewBox: '0, 0, 400, 300'
+  });
 
-  mainElement.appendChild(svg);
+  var box = svg.rect(10, 10, 100, 100);
+
+  svg.fill(box, '#EE9999');
+  svg.stroke(box, 'black', 3);
+
+  canvas.appendChild(box);
+
+  mainElement.appendChild(canvas);
+};
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/**
+ * Convert from camel notation to kebab notation.
+ * @param {string} camel 
+ */
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var camelToKebab = function camelToKebab(camel) {
+
+  var pattern = /([A-Z]{1})/;
+
+  var replacer = function replacer(match, capital, offset, str) {
+    return '-' + capital.toLowerCase(capital);
+  };
+
+  while (camel.search(pattern) != -1) {
+    camel = camel.replace(pattern, replacer);
+  }
+
+  return camel;
+};
+
+/**
+ * Add given attributes to given SVG element.
+ * @param {SVGElement} elem 
+ * @param {Object} attributes 
+ */
+var addAttributes = function addAttributes(elem, attributes) {
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = Object.entries(attributes)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var _step$value = _slicedToArray(_step.value, 2),
+          key = _step$value[0],
+          value = _step$value[1];
+
+      elem.setAttributeNS(null, camelToKebab(key), value);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+};
+
+/**
+ * Stroke an element. color and width overrides those specified in strokeProps.
+ * @param {SVGElement or Array} elem
+ * @param {string} color 
+ * @param {number} width 
+ * @param {Object} strokeProps 
+ */
+var stroke = exports.stroke = function stroke(elem, color, width, strokeProps) {
+  if (typeof elem === 'undefined') {
+    return;
+  }
+
+  var elems = [];
+  if (!Array.isArray(elem)) {
+    elems.push(elem);
+  } else {
+    elems = elem;
+  }
+
+  if (typeof strokeProps === 'undefined') {
+    strokeProps = {};
+  }
+
+  if (typeof color !== 'undefined') {
+    strokeProps.stroke = color;
+  }
+
+  if (typeof width !== 'undefined') {
+    strokeProps.strokeWidth = width;
+  }
+
+  elems.forEach(function (el) {
+    addAttributes(el, strokeProps);
+  });
+};
+
+/**
+ * Fill an element
+ * @param {SVGElement} elem
+ * @param {string} color 
+ * @param {Object} fillProps 
+ */
+var fill = exports.fill = function fill(elem, color, fillProps) {
+  if (typeof elem === 'undefined') {
+    return;
+  }
+
+  var elems = [];
+  if (!Array.isArray(elem)) {
+    elems.push(elem);
+  } else {
+    elems = elem;
+  }
+
+  if (typeof fillProps === 'undefined') {
+    fillProps = {};
+  }
+
+  if (typeof color !== 'undefined') {
+    fillProps.fill = color;
+  }
+
+  elems.forEach(function (el) {
+    addAttributes(el, fillProps);
+  });
+};
+
+/**
+ * Create a rectangle.
+ * @param {number} x 
+ * @param {number} y 
+ * @param {number} w 
+ * @param {number} h 
+ */
+var rect = exports.rect = function rect(x, y, w, h) {
+  return createSVGElement('rect', {
+    x: x, y: y, width: w, height: h
+  });
 };
 
 /**
  * Create an SVG element with proper namespace.
  * @param {string} tag 
+ * @param {Object} attributes
  */
-var createSVGElement = exports.createSVGElement = function createSVGElement(tag) {
+var createSVGElement = exports.createSVGElement = function createSVGElement(tag, attributes) {
   var svgns = 'http://www.w3.org/2000/svg';
   // let xlinkns = 'http://www.w3.org/1999/xlink';
 
-  return document.createElementNS(svgns, tag);
+  var elem = document.createElementNS(svgns, tag);
+
+  if (typeof attributes !== 'undefined' && (typeof attributes === 'undefined' ? 'undefined' : _typeof(attributes)) === 'object') {
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+      for (var _iterator2 = Object.entries(attributes)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        var _step2$value = _slicedToArray(_step2.value, 2),
+            key = _step2$value[0],
+            value = _step2$value[1];
+
+        elem.setAttributeNS(null, key, value);
+      }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2.return) {
+          _iterator2.return();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
+      }
+    }
+  }
+
+  return elem;
 };
 
 /***/ })
